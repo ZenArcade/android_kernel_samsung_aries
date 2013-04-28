932409daed26518ccff2a06df27f14d73e5d949b
scotthartbti <scott.hart.bti@gmail.com>
2013-04-27 22:33:02 -0400

    Files for infuse mtd and partitions Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- drivers/mtd/onenand/samsung.c
+++ drivers/mtd/onenand/samsung.c
@@ -48,6 +48,8 @@ enum soc_type {
 #include "samsung_fascinate.h"
 #elif defined(CONFIG_MACH_P1)
 #include "samsung_p1.h"
+#elif defined(CONFIG_SAMSUNG_INFUSE)
+#include "samsung_infuse4g.h"
 #else
 #error Should not be used on aries devices (can brick!).
 #include "samsung.h"
